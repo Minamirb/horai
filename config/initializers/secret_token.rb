22e8d0ed1d5649cf00d9ef796ff7fec27c37768f
yalab <rudeboyjet@gmail.com>
2011-06-19 22:57:46 +0900

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Horai::Application.config.secret_token = 'c7fb6021baed222f97b177610192e72418a68b35b36d9e1b012b32458a8a8d55f55422e1177d4828082ed16e94c9ccb3c509f1118b74c3fe08634fa47c28add0'
