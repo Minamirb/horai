68dda63775810d2c1f366f644f940b38b89dfaab
yalab <rudeboyjet@gmail.com>
2011-06-20 16:53:25 +0900

    デザインを整えた

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Horai::Application.routes.draw do
-  get "root/index"
-
-  resources :users
+  root :to => 'root#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -49,10 +47,6 @@ Horai::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
